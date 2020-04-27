70b3199c284823546f5a896e7b0f2256e4c18e71
Fung <koufongso@gmail.com>
2020-04-26 22:37:15 -0700

    built react app

---
+++ client/build/precache-manifest.56105f8a7556537acc970a5180816956.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "95efc76a9ce3b928dbae",
+    "url": "/static/css/main.79584cb0.chunk.css"
+  },
+  {
+    "revision": "95efc76a9ce3b928dbae",
+    "url": "/static/js/main.0592577c.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "37fb632cbac90486d340",
+    "url": "/static/js/2.dfdd3e42.chunk.js"
+  },
+  {
+    "revision": "0c6d8ce2701337bb16185c33c697055b",
+    "url": "/index.html"
+  }
+];
